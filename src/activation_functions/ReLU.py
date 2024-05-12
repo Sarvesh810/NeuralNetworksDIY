c3c5f71b64f6e5c49e4c74130fd40b7b4784ffbe
Sarvesh Khandelwal <sc21sk@leeds.ac.uk>
2024-05-12 22:36:53 +0100

    Added ReLU activation function

---
+++ src/activation_functions/ReLU.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# I will be using Rectified Linear Unit (ReLU) as the activation function for the hidden layers
+# Why ReLU? Because it doesn't have the problem of vanishing gradient and it's derivative/gradient
+# is very easy and fast to compute. Although vanishing gradient problem usually occurs in
+# deep neural nets which probably would not be the case here
+class ReLU():
+    def forward(self, input: np.ndarray) -> None:
+        # Input <= 0 ---> Output = 0
+        # IInput > 0 ---> Output = Input
+        self.output = np.maximum(0, input)
