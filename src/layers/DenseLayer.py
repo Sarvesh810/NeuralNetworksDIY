309d685ad3c21b60a86ec79395acb98379574253
Sarvesh Khandelwal <sc21sk@leeds.ac.uk>
2024-05-12 20:39:01 +0100

    Added comments to DenseLayer class

--- src/layers/DenseLayer.py
+++ src/layers/DenseLayer.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+# A Dense layer is connected with all the neurons from the previous layer
 class DenseLayer():
     def __init__(self, no_inputs:int, no_neurons: int) -> None:
         # Take for example input array has 3 parameters/data-points and we want this layer to have 5 neurons
@@ -11,4 +11,12 @@ class DenseLayer():
         self.biases = np.random.randn(no_neurons)
 
     def foward(self, input: np.ndarray):
+        # The activation (value) of each neuron is the sum of (activations*weights) + bias
+        # Activation is the output of neuron
+        # Weight gives how much importance to give to each neuron in previous layer
+        # Biases help in a way de-localizing the function. Assume what 'b' does in equation 'y = mx + b'
         self.output = np.dot(input, self.weights) + self.biases
+
+### These calculations use Numpy which uses CPU to make calculations
+### Leveraging GPUs can make the training process faster
+### This can be done by using the tensors from TensorFlow library
