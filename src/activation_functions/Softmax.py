81ef201f69dbee28bfda45434bcbb807459a0d46
Sarvesh Khandelwal <sc21sk@leeds.ac.uk>
2024-05-13 17:46:39 +0100

    Added Softmax activation layer

---
+++ src/activation_functions/Softmax.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# We are using Softmax Activation layer after the output layer because we want a
+# probability distribution of different possible outputs. This will give us a way of comparing
+# how correct we are for each sample in the data
+class Softmax():
+    def forward(self, inputs: np.ndarray):
+        # (e^zi)/sum(e^zj) where 'i' is i-th output and j=1...k
+        # e -> Euler's number
+        exponents = np.exp(inputs - np.max(inputs))
+        self.output = exponents/np.sum(exponents)
