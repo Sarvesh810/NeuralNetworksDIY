99b692a88fbe6e43f3c584372a98135af5083069
Sarvesh Khandelwal <sarveshkhandelwal710@gmail.com>
2024-05-14 09:17:37 +0100

    Added LeakyReLU activation fucntion

---
+++ src/activation_functions/LeakyReLU.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Leaky ReLU solves the problem of dying neurons in ReLU. I believe that if a network
+# trains over many epochs, there is a chance of neurons dying due to ReLU's gradient becoming 0
+class LeakyReLU():
+    def forward(self, inputs: np.ndarray) -> None:
+        # Input <= 0 ---> Output = Input*leakage
+        # Input > 0 ---> Output = Input
+        self.leakage = 0.1
+        self.output = np.maximum(inputs*self.leakage, inputs)
