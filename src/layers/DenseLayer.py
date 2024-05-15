2808a9c14423e80402ca908d6c2942d477bf9986
Sarvesh Khandelwal <sarveshkhandelwal710@gmail.com>
2024-05-15 17:19:18 +0100

    Added backward function for DenseLayer

--- src/layers/DenseLayer.py
+++ src/layers/DenseLayer.py
@@ -10,9 +10,24 @@ class DenseLayer():
         # Each neuron has 1 bias, continuing our example 5 neurons in a layer will have 5 biases
         self.biases = np.random.randn(no_neurons)
 
+        # Learning rate decides how big the steps will be while adjusting the parameters
+        self.learning_rate = 0.1
+
     def forward(self, inputs: np.ndarray) -> None:
         # The activation (value) of each neuron is the sum of (activations*weights) + bias
         # Activation is the output of neuron
         # Weight gives how much importance to give to each neuron in previous layer
         # Biases help in a way de-localizing the function. Assume what 'b' does in equation 'y = mx + b'
-        self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+        self.output = np.dot(self.inputs, self.weights) + self.biases
+
+    def backward(self, gradients: np.ndarray) -> None:
+        # It can be divided into 2 steps:
+        # 1. Calculating the gradients for inputs, weights and biases
+        self.d_inputs = np.dot(gradients, self.weights.T)
+        self.d_weights = np.dot(self.inputs, gradients)
+        self.d_biases = np.sum(gradients, axis=0, keepdims=True)
+
+        # 2. Updating those weights and biases
+        self.weights -= self.learning_rate*self.d_weights
+        self.biases -= self.learning_rate*self.d_biases
