caf603854ac29084106cde72f6bb878028158ecd
Sarvesh Khandelwal <sarveshkhandelwal710@gmail.com>
2024-05-18 00:11:16 +0100

    Added train function for NeuralNetwork

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -15,27 +15,40 @@ class NeuralNetwork():
 
     # Forward method for the whole network
     def forward(self, inputs: np.ndarray) -> np.ndarray:
+        # Go over every layer in the model
         for layer in self.layers:
             inputs = layer.forward(inputs)
         return inputs
     
-    # Backwward mehtod for the whole network
-    def backward(self, gradients: np.ndarray, learning_rate: float):
+    # Backward method for the whole network
+    def backward(self, gradients: np.ndarray, learning_rate: float) -> None:
         for layer in self.layers[::-1]:
             layer.backward(gradients)
             gradients = layer.gradients
 
-    def train(self, X_train: np.ndarray, Y_train: np.ndarray, epochs: int, batch_size: int, learning_rate: float=0.01):
-        # The data is shuffled and fed into the model quite a few times
-        # This helps in better learning of the model
+    def train(self, X_train: np.ndarray, Y_train: np.ndarray, epochs: int, batch_size: int, learning_rate: float=0.01) -> None:
+        # An epoch is complete when the neural network gas gone over the data once. We repeat this
+        # process of the neural network going over the dataset many times, shuffling the data
+        # each time to make it learn more from the same data
         for epoch in range(epochs):
-            indices = np.arange(X_train.shape[0])
+            indices = np.arange(X_train.shape[0])       # Similar to python's range() method
             np.random.shuffle(indices)
             X_train = X_train[indices]
             Y_train = Y_train[indices]
 
-            # Loop over the data batch-wise and train the model
-            for batch in range(0, X_train, batch_size):
-                pass
-        # Training porcess can be divided into 3 steps:
-        # 1. Forward Step
+            # Now, for each epoch, divide the data into batches
+            # Each batch is fed as a single input to the network
+            # This speeds up the learning process with a tradeoff with some accuracy
+            for batch in range(0, X_train.shape[0], batch_size):
+                X = X_train[batch:batch+batch_size]     # Slice the data in a batch iterarting over batches
+                Y = Y_train[batch:batch+batch_size]
+                
+                # Now, the training process can be divided into 2 steps:
+                # Step 1: Forward Propagation and calculate loss
+                output = self.forward(inputs=X)
+                loss = self.loss_function.forward(true_values=Y, predicted_values=output)
+
+                # Step 2: Calculate gradients w.r.t loss and Backward Propagation
+                gradients = self.loss_function.backward(true_values=Y, predicted_values=output)
+                self.backward(gradients=gradients)
+
