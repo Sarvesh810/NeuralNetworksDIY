a5311643d78814559621c01531e5556b7e03c33e
Sarvesh Khandelwal <sarveshkhandelwal710@gmail.com>
2024-05-17 15:13:04 +0100

    Refactored LeakyReLU

--- src/activation_functions/LeakyReLU.py
+++ src/activation_functions/LeakyReLU.py
@@ -9,7 +9,9 @@ class LeakyReLU():
         self.leakage = 0.1
         self.inputs = inputs
         self.output = np.maximum(inputs*self.leakage, inputs)
+        return self.output
 
     def backward(self, gradients: np.ndarray):
         leaky_relu_gradients = np.where(gradients > 0, 1, self.leakage)
         self.gradients = gradients * leaky_relu_gradients
+        return self.gradients
